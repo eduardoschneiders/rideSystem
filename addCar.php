cbe61d67161d620804e29a2839521757a0e9081f
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-09-08 02:30:46 -0300

    Página addCar selectiona usuário com select

--- addCar.php
+++ addCar.php
@@ -2,15 +2,26 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
 
+include ("Includes/Classes/Person.class.php");
+include ("Includes/config.php");
+
+$person = new Person();
+
+$all_people = $person->selectAll();
+
+while ($thePerson = mysql_fetch_array($all_people)) {
+	$person_options .= '<option  value="' . $thePerson['idPessoa'] . '">' . $thePerson['nome'] . '</option>';
+}
+
 ?>
 
 <form method="post" action="take_values_car.php">
-	Pessoa: <input type="text" name="idPerson" </input></br>
-	Placa:	  <input type="text" name="plate"></input></br>
-	Descrição: <input type="text" name="description"></input></br>
-	Ano:	  <input type="text" name="year"></input></br>
-	Cor:	  <input type="text" name="color"></input></br>
-	Foto:	  <input type="text" name="photo"></input></br>
+	Pessoa: 	<select name="idPerson"><?php echo $person_options ?></select></br>
+	Placa:	  	<input type="text" name="plate"></input></br>
+	Descrição: 	<input type="text" name="description"></input></br>
+	Ano:	  	<input type="text" name="year"></input></br>
+	Cor:	 	<input type="text" name="color"></input></br>
+	Foto:	  	<input type="text" name="photo"></input></br>
 
 	<input type="submit" name="save" value="save"></input>
 </form>
