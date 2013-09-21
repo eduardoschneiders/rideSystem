322d0f0fd12a9f65b6b843f96e54def5bb802b67
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-09-20 23:34:05 -0300

    UniqueId method

--- Includes/Classes/Util.class.php
+++ Includes/Classes/Util.class.php
@@ -4,4 +4,8 @@ class Util{
   public function redirect($pag){
     echo '<script type="text/javascript">document.location.href="'.$pag.'";</script>';
   }
+
+  public function uniqId(){
+    return md5(microtime());
+  }
 }
