ce7852c57acab102425af4957227c57f9ffdd560
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-09-21 20:59:09 -0300

    Calculating number of available seats

--- showTravels.php
+++ showTravels.php
@@ -1,6 +1,7 @@
 
 <?php
 include ("Includes/Classes/Travel.class.php");
+include ("Includes/Classes/Ride.class.php");
 include ("Includes/Classes/City.class.php");
 include ("Includes/Classes/Person.class.php");
 include ("Includes/config.php");
@@ -8,6 +9,7 @@ include ("Includes/config.php");
 $travel = new Travel();
 $city = new City();
 $person = new Person();
+$ride = new Ride();
 
 $html = new Html();
 echo $html->header();
@@ -19,6 +21,7 @@ foreach ($travels as $key => $travel) {
 	$from = $city->find($travel['idMunicipioDestino']);
 	$to = $city->find($travel['idMunicipioOrigem']);
 	$driver = $person->find($travel['idPessoaMotorista']);
+  $availableSeats = $travel['assentosDisponiveis'] - count($ride->find(array('idViagem' => $travel['idViagem'])));
 
 	$travels_item .= '
 					<li>
@@ -26,7 +29,7 @@ foreach ($travels as $key => $travel) {
               <b>Até:</b> ' . $to[0]['nome'] . '
               <b>Motorista:</b> ' . $driver[0]['nome'] . '
               <b>Partida:</b> ' . $travel['dataDePartida'] . ' ' . $travel['horarioDePartida'] . '
-              <b>Lugares Disponíveis:</b> ' . $travel['assentosDisponiveis'] . '
+              <b>Lugares Disponíveis:</b> ' . $availableSeats  . '
             <a href="takeRide.php?travel=' . $travel['idViagem'] . '">
               Carona
             </a>
