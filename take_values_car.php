e5aac0d12ad2f60499be78eba8312749ce955504
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-09-28 18:19:31 -0300

    take values car page #21

--- take_values_car.php
+++ take_values_car.php
@@ -4,38 +4,47 @@
   include ("Includes/Classes/Car.class.php");
   include ("Includes/Classes/Upload.class.php");
 
-
-
   //values of Cars
+  $idCar = $_POST["idCar"];
   $idPerson = $_POST["idPerson"];
   $plate = $_POST["plate"];
   $description = $_POST["description"];
   $year = $_POST["year"];
   $color = $_POST["color"];
-  $photo = $_POST["photo"];
+  $photo = $_FILES["photo"];
 
-  $upload = new Upload();
-  $uniqId = Util::uniqId();
-  $car = new Car();
-  $nextCarId = $car->getLastId() + 1;
-  $userName = $_SESSION['userName'];
+   $valuesCar = array(
+              'idPessoa'      => $idPerson,
+              'placa'         => $plate,
+              'descricao'     => $description,
+              'ano'           => $year,
+              'cor'           => $color,
+              // 'fotografia'    => $upload->get_file_name()
+          );
 
-  $fileName = $nextCarId . '_' .  $userName . '_' . $uniqId;
-  $upload->allowed_file_dimensions  = array('width' => 300, 'height' => 250);
-  if ($upload->upload_file($_FILES["photo"]["tmp_name"], 'Photos/Cars/', $fileName)){
+  if($photo['size']){
 
+    $upload = new Upload();
+    $uniqId = Util::uniqId();
+    $car = new Car();
+
+    $nextCarId = $car->getLastId() + 1;
+    $userName = $_SESSION['userName'];
+
+    $fileName = $nextCarId . '_' .  $userName . '_' . $uniqId;
+    $upload->allowed_file_dimensions  = array('width' => 300, 'height' => 250);
+    if ($upload->upload_file($photo["tmp_name"], 'Photos/Cars/', $fileName)){
+      $valuesCar['fotografia'] = $upload->get_file_name();
+    }
   }
 
-    $car = new Car(
-                    array(
-                        'idPessoa'      => $idPerson,
-                        'placa'         => $plate,
-                        'descricao'     => $description,
-                        'ano'           => $year,
-                        'cor'           => $color,
-                        'fotografia'    => $upload->get_file_name()
-                    )
-                );
 
-    $car->insert();
-    Util::redirect('showTravels.php');
+  $car = new Car(
+            $valuesCar
+  );
+
+
+  $car->valuePK = $idCar;
+
+  $car->save();
+  Util::redirect('showCar.php');
