cd6b934d85a554597ccbc27ee8b09d2a6d1fd7dc
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-09-28 23:50:20 -0300

    Selected menu hover efect

--- Includes/Classes/Html.class.php
+++ Includes/Classes/Html.class.php
@@ -51,14 +51,45 @@
     private function headerArea(){
       session_start();
 
+      switch (Util::getPage()) {
+        case 'index.php':
+          $active['index'] = 'active';
+          break;
+
+        case 'showTravels.php':
+          if($_SERVER['QUERY_STRING'])
+            $active['showTravels'] = 'active';
+          else
+            $active['index'] = 'active';
+
+          break;
+
+        case 'showCar.php':
+          $active['showCar'] = 'active';
+          break;
+
+        case 'addTravel.php':
+          $active['addTravel'] = 'active';
+          break;
+
+        case 'addCar.php':
+          $active['addCar'] = 'active';
+          break;
+
+        default:
+          # code...
+          break;
+      }
+
       if($_SESSION['logged']){
         $login = '<li><img src="Photos/People/' . $_SESSION["userPhoto"] . '"></li>
-                  <li><a href="addTravel.php">Cadastrar Viagem</a></li>
-                  <li><a href="addCar.php">Cadastrar Carro</a></li>';
+                  <li class="' . $active['addTravel'] . '"><a href="addTravel.php">Cadastrar Viagem</a></li>
+                  <li class="' . $active['addCar'] . '"><a href="addCar.php">Cadastrar Carro</a></li>';
       }else{
         $login = '<li><a href="addPerson.php">Cadastrar-se</a></li>
                   <li><a href="login.php">Login</a></li>';
       }
+
 
       $html = '
         <header>
@@ -70,8 +101,9 @@
                 </a>
                 <div class="nav-collapse">
                   <ul class="nav">
-                    <li class="active"><a href="./">Hommer</a></li>
-                    <li><a href="showTravels.php?driver=' . $_SESSION['userId'] . '">Minhas Caronas</a></li>
+                    <li class="' . $active['index'] . '"><a href="./">Hommer</a></li>
+                    <li class="' . $active['showTravels'] . '"><a href="showTravels.php?driver=' . $_SESSION['userId'] . '">Minhas Caronas</a></li>
+                    <li class="' . $active['showCar'] . '"><a href="showCar.php">Meu Carro</a></li>
                     <li><a href="#about">Sobre</a></li>
                     <li><a href="#contact">Contato</a></li>
                   </ul>
