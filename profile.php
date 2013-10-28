44152c5db1cce7fdab4aaea3a8107fde5b2f87c5
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-10-28 21:47:18 -0200

    Alterado perfil

--- profile.php
+++ profile.php
@@ -2,27 +2,39 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
 include ("Includes/Classes/City.class.php");
+include ("Includes/Classes/Person.class.php");
 include ("Includes/config.php");
 
 $city = new City();
+$person = new Person();
 $html = new Html();
+
+$idPerson = $_GET['idPerson'];
+if ($idPerson){
+	$thePerson = $person->find($idPerson);
+	Util::pr($thePerson);
+}
 
 echo $html->header();
 
-$all_cities = $city->find();
+foreach ($city->find() as $key => $city) {
 
-foreach ($all_cities as $key => $city) {
-	$city_options .= '<option  value="' . $city['idMunicipio'] . '">' . $city['nome'] . '</option>';
+	$selected = '';
+	if ($city['idMunicipio'] == $thePerson[0]['idMunicipio'])
+		$selected = 'selected="selected"';
+
+	$city_options .= '<option ' . $selected . ' value="' . $city['idMunicipio'] . '">' . $city['nome'] . '</option>';
 }
 
 ?>
 
 <form method="post" action="take_values_person.php" enctype="multipart/form-data">
 
-	Cidade:<select name="city">
-	<?php echo $city_options ?>
-	</select></br>
-	name: <input type="text" name="name" id="name"><br/>
+	Cidade:
+		<select name="city">
+		<?php echo $city_options ?>
+		</select></br>
+	name: <input type="text" name="name" id="name" value="<?php echo $thePerson['nome']?>"><br/>
 	email: <input type="text" name="email" id="email"><br/>
 	password: <input type="password" name="password" id="password"><br/>
 	retype password: <input type="password" name="retypePassword" id="retypePassword"><br/>
